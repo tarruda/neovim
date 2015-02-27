36473aac17a5c8acf21cd2f2e7674c2f54375f81
Thiago de Arruda <tpadilha84@gmail.com>
2015-02-26 23:29:08 -0300

    job: Fix process cleanup using SIGCHLD/waitpid
    Use only one SIGCHLD handler for all pty processes, passing the WNOHANG flag.

--- src/nvim/os/pty_process.c
+++ src/nvim/os/pty_process.c
@@ -6,7 +6,6 @@
 #include <unistd.h>
 #include <termios.h>
 #include <sys/types.h>
-#include <sys/wait.h>
 #include <sys/ioctl.h>
 
 // forkpty is not in POSIX, so headers are platform-specific
@@ -33,13 +32,13 @@
 typedef struct {
   struct winsize winsize;
   uv_pipe_t proc_stdin, proc_stdout, proc_stderr;
-  uv_signal_t schld;
   int tty_fd;
 } PtyProcess;
 
 void pty_process_init(Job *job)
 {
   PtyProcess *ptyproc = xmalloc(sizeof(PtyProcess));
+  ptyproc->tty_fd = -1;
 
   if (job->opts.writable) {
     uv_pipe_init(uv_default_loop(), &ptyproc->proc_stdin, 0);
@@ -102,9 +101,6 @@ bool pty_process_spawn(Job *job)
     uv_pipe_open(&ptyproc->proc_stderr, dup(master));
   }
 
-  uv_signal_init(uv_default_loop(), &ptyproc->schld);
-  uv_signal_start(&ptyproc->schld, chld_handler, SIGCHLD);
-  ptyproc->schld.data = job;
   ptyproc->tty_fd = master;
   job->pid = pid;
   return true;
@@ -112,11 +108,18 @@ bool pty_process_spawn(Job *job)
 
 void pty_process_close(Job *job)
 {
-  PtyProcess *ptyproc = job->process;
-  uv_signal_stop(&ptyproc->schld);
-  uv_close((uv_handle_t *)&ptyproc->schld, NULL);
+  pty_process_close_master(job);
   job_close_streams(job);
   job_decref(job);
+}
+
+void pty_process_close_master(Job *job)
+{
+  PtyProcess *ptyproc = job->process;
+  if (ptyproc->tty_fd != -1) {
+    close(ptyproc->tty_fd);
+    ptyproc->tty_fd = -1;
+  }
 }
 
 void pty_process_resize(Job *job, uint16_t width, uint16_t height)
@@ -144,31 +147,6 @@ static void init_child(Job *job)
   setenv("TERM", job->opts.term_name ? job->opts.term_name : "ansi", 1);
   execvp(job->opts.argv[0], job->opts.argv);
   fprintf(stderr, "execvp failed: %s\n", strerror(errno));
-}
-
-static void chld_handler(uv_signal_t *handle, int signum)
-{
-  Job *job = handle->data;
-  int stat = 0;
-
-  if (waitpid(job->pid, &stat, 0) < 0) {
-    fprintf(stderr, "Waiting for pid %d failed: %s\n", job->pid,
-        strerror(errno));
-    return;
-  }
-
-  if (WIFSTOPPED(stat) || WIFCONTINUED(stat)) {
-    // Did not exit
-    return;
-  }
-
-  if (WIFEXITED(stat)) {
-    job->status = WEXITSTATUS(stat);
-  } else if (WIFSIGNALED(stat)) {
-    job->status = WTERMSIG(stat);
-  }
-
-  pty_process_close(job);
 }
 
 static void init_termios(struct termios *termios)
