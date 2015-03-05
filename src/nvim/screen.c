e073778ce9221f1ad1c6408253f2e44098e46181
Thiago de Arruda <tpadilha84@gmail.com>
2015-03-05 18:07:44 -0300

    terminal: Implement the :terminal ex command

--- src/nvim/screen.c
+++ src/nvim/screen.c
@@ -2595,6 +2595,13 @@ win_line (
     off += col;
   }
 
+  // does anyone ever need a terminal with more than 1024 columns?
+  int term_attrs[1024] = {0};
+  if (wp->w_buffer->terminal) {
+    terminal_get_line_attributes(wp->w_buffer->terminal, lnum, term_attrs);
+    extra_check = true;
+  }
+
   /*
    * Repeat for the whole displayed line.
    */
@@ -3216,6 +3223,8 @@ win_line (
             syntax_flags = 0;
           else
             syntax_flags = get_syntax_info(&syntax_seqnr);
+        } else if (!attr_pri) {
+          char_attr = 0;
         }
 
         /* Check spelling (unless at the end of the line).
@@ -3293,6 +3302,11 @@ win_line (
           else
             char_attr = hl_combine_attr(spell_attr, char_attr);
         }
+
+        if (wp->w_buffer->terminal) {
+          char_attr = hl_combine_attr(char_attr, term_attrs[vcol]);
+        }
+
         /*
          * Found last space before word: check for line break.
          */
@@ -3790,6 +3804,18 @@ win_line (
         }
       }
 
+      if (wp->w_buffer->terminal) {
+        // terminal buffers may need to highlight beyond the end of the
+        // logical line
+        while (col < wp->w_width) {
+          ScreenLines[off] = ' ';
+          if (enc_utf8) {
+            ScreenLinesUC[off] = 0;
+          }
+          ScreenAttrs[off++] = term_attrs[vcol++];
+          col++;
+        }
+      }
       SCREEN_LINE(screen_row, wp->w_wincol, col, wp->w_width, wp->w_p_rl);
       row++;
 
@@ -6539,7 +6565,8 @@ int showmode(void)
   int sub_attr;
 
   do_mode = ((p_smd && msg_silent == 0)
-             && ((State & INSERT)
+             && ((State & TERM_FOCUS)
+                 || (State & INSERT)
                  || restart_edit
                  || VIsual_active
                  ));
@@ -6595,7 +6622,9 @@ int showmode(void)
           }
         }
       } else {
-        if (State & VREPLACE_FLAG)
+        if (State & TERM_FOCUS) {
+          MSG_PUTS_ATTR(_(" TERMINAL"), attr);
+        } else if (State & VREPLACE_FLAG)
           MSG_PUTS_ATTR(_(" VREPLACE"), attr);
         else if (State & REPLACE_FLAG)
           MSG_PUTS_ATTR(_(" REPLACE"), attr);
